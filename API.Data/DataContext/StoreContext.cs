b203963483ec3a0ae0aa2004598f46872a6b8125
sukruuzun2023 <developeryesteknoloji@gmail.com>
2022-04-15 23:39:52 +0300

    git -first

---
+++ API.Data/DataContext/StoreContext.cs
@@ -0,0 +1,17 @@
+﻿using API.Data.DbModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data.DataContext
+{
+    public class StoreContext : DbContext // Db Context Sınıfından Almak zorunda
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer("server=94.73.146.3;database=u0625128_Core;User = u0625128_user01;Password =XMxe96O6YKwc16E");
+            optionsBuilder.UseSqlServer("server=DESKTOP-DR83AEK\\SQLEXPRESS;database=StoreContext;integrated security=true");
+
+        }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using API.Data.DataContext;
+using API.Data.DbModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context; // Bir Değişken olarak tanımladık
+        public ProductsController(StoreContext context) // ctor yazarak constarak olusturduk 
+        {
+            _context = context; // tanımladığım store context store context mı ver
+
+        }
+
+
+        [HttpGet]
+        public async Task< ActionResult<List<Product>>> GetProducts() // Senin geri dönüş tipin ActionResult olcak ama birden cok data dönceği için list olcak bu liste de product listesi olsun diyoruz
+        {
+            var data = await _context.Products.ToListAsync();
+            return data;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id) // sem geri dönüş olarak Product içersinde bir id numarası al
+        {
+            return await _context.Products.FindAsync(id);  // bana context içersinde buldugun id numarasını göster
+        }
+    }
+}
